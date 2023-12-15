c9c751f72249309cf475a390c8caf2f234f6921f
sebastienlorber <lorber.sebastien@gmail.com>
2023-12-15 23:56:25 +0100

    Add GitHub pages deployment config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,15 +9,15 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://stylexjs.com",
+  url: "https://nmn.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/docusaurus-stylex/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "stylex", // Usually your repo name.
+  organizationName: "nmn", // Usually your GitHub org/user name.
+  projectName: "docusaurus-stylex", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
